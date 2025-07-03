334d43e4ee13eb51ee4b488448500cd7d8c9ceee
MIBINGO286 <tokiovalencia20@gmail.com>
2025-07-02 22:45:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbzNMTwYH6URiHKx26XPph2prcwyO22p6GyIn49IsfA2dr2yv3rNBinTz3Es2lFw77VUFQ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwG-U5U7jrxHM2yUwkp3cc2uDbBpAzYDvezv7vNfwHHQamyXYEau6mbiJIYjVCkHVXp/exec';
 const CARDS_JSON = 'bingo_cards.json';
 const WHATSAPP_PHONE = '584266404042';
 const ADMIN_PASSWORD = 'Jrr035$$*';
